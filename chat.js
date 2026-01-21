eccdd24564e96b584d5fb49389913c645581dc99
waitheyo16 <waitheyo16@gmail.com>
2026-01-21 17:58:45 +0900

    client

---
+++ chat.js
@@ -0,0 +1,17 @@
+varsocket=io();
+varmessages=document.getElementById('messages');
+varform=document.getElementById('form');
+varinput=document.getElementById('input');
+form.addEventListener('submit',function(e){
+e.preventDefault();
+if(input.value){
+socket.emit('chatmessage',input.value);
+input.value='';
+}
+});
+socket.on('chatmessage',function(msg){
+varitem=document.createElement('li');
+item.textContent=msg;
+messages.appendChild(item);
+window.scrollTo(0,document.body.scrollHeight);
+});
