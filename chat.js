ffb37735d9d8c806d2aaa2947ebe671c7c29bf88
waitheyo16 <waitheyo16@gmail.com>
2026-01-22 16:58:51 +0900

    double quote

--- chat.js
+++ chat.js
@@ -5,12 +5,12 @@ const input=document.getElementById('input');
 form.addEventListener('submit',function(e){
 e.preventDefault();
 if(input.value){
-socket.emit('chatmessage',input.value);
-input.value='';
+socket.emit("chatmessage",input.value);
+input.value="";
 }
 });
-socket.on('chatmessage',function(msg){
-varitem=document.createElement('li');
+socket.on("chatmessage",function(msg){
+varitem=document.createElement("li");
 item.textContent=msg;
 messages.appendChild(item);
 window.scrollTo(0,document.body.scrollHeight);
